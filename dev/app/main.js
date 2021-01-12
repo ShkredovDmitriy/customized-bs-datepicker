207c9805db7fa31d25ca6c4b30325d1fcee0f017
Dmitriy Shkredov <shkredovdmitriy@gmail.com>
2021-01-12 13:16:15 +0300

    Experements

--- dev/app/main.js
+++ dev/app/main.js
@@ -9,3 +9,23 @@ $(".custom-datepicker").datepicker({
     rightArrow: "<span></span>"
   }
 });
+
+$(".custom-datepicker").datepicker("update", new Date());
+
+$(".custom-datepicker__prev").on("click", () => {
+  console.log("nnn");
+});
+
+$(".custom-datepicker")
+  .datepicker()
+  .on("changeMonth", function(e) {
+    var currMonth = new Date(e.date);
+    console.log(currMonth);
+  });
+
+$(".custom-datepicker")
+  .datepicker()
+  .on("changeYear", function(e) {
+    var currYear = String(e.date);
+    console.log(currYear);
+  });
